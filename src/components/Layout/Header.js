7f23ebff50f0add3e7b3ba721d29c73d42fc978e
Brian Kim <briaansjkim@gmail.com>
2020-10-06 18:03:33 -0400

    working on styling header

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -7,13 +7,15 @@ import Logo from '../../assets/images/HonLogo.png';
 
 const Header = () => (
     <header css={tw`sticky top-0 bg-white shadow`}>
-        <div>
-            <img
-                css={tw`mx-auto`}
-                src={Logo}
-                alt="logo"
-                style={{ height: '40px' , width: '60px'}}
-            />
+        <div css={tw`container flex flex-col sm:flex-row justify-between items-center mx-auto py-4 px-8`}>
+            <div css={tw`flex items-center text-3xl`}>
+                <img
+                    css={tw`mx-auto`}
+                    src={Logo}
+                    alt="logo"
+                    style={{ height: '40px' , width: '60px'}}
+                />
+            </div>
         </div>
     </header>
 );
