b6653ca17b8bd9e7c4649e2503c6cab71512c86c
Brian Kim <briaansjkim@gmail.com>
2020-10-08 15:05:03 -0400

    finished creating and styling footer

--- src/components/Layout/Footer.js
+++ src/components/Layout/Footer.js
@@ -1,7 +1,7 @@
 import React from "react";
 import tw from "tailwind.macro";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faFacebook, faInstagram, faLinkedin } from '@fortawesome/free-brands-svg-icons';
+import { faFacebookF, faInstagram, faLinkedinIn } from '@fortawesome/free-brands-svg-icons';
 
 import Logo from "../../assets/images/HonLogo.png";
 
@@ -36,7 +36,7 @@ const Footer = () => (
                         target="_blank"
                         rel="noopener noreferrer"
                         aria-label="Instagram"
-                        css={tw`text-black bg-gray-200 text-2xl`}
+                        css={tw`mx-2 p-2 text-black rounded-full bg-gray-400 text-md`}
                         ><FontAwesomeIcon icon={faInstagram} />
                     </a>
                     <a
@@ -44,66 +44,20 @@ const Footer = () => (
                         target="_blank"
                         rel="noopener noreferrer"
                         aria-label="LinkedIn"
-                        css={tw`text-black bg-gray-200 text-2xl`}
-                        ><FontAwesomeIcon icon={faLinkedin} />
+                        css={tw`mx-2 p-2 text-black rounded-full bg-gray-400 text-md`}
+                        ><FontAwesomeIcon icon={faLinkedinIn} />
                     </a>
                     <a
                         href="https://www.facebook.com/HonMediaSD/"
                         className="text-button hover:text-buttonHover"
                         target="_blank"
                         rel="noopener noreferrer"
-                        css={tw`text-black bg-gray-200 text-2xl`}
-                        ><FontAwesomeIcon icon={faFacebook} />
+                        css={tw`mx-2 p-2 text-black rounded-full bg-gray-400 text-md`}
+                        ><FontAwesomeIcon icon={faFacebookF} />
                     </a>
             </div>
         </div>
     </footer>
-  // <footer className="container mx-auto py-16 px-3 mt-16 mb-8 text-gray-800">
-  //     <div className="flex -mx-3">
-  //         <div className="flex-1 px-3">
-  //             <h2 className="text-2xl font-semibold">Social Media</h2>
-  //             <ul className="mt-4 leading-loose">
-  //             <i className="fa fa-instagram" aria-hidden="true" />
-  //             <li>
-  //                 <a
-  //                 href="https://www.instagram.com/honmediasd/"
-  //                 className="text-button hover:text-buttonHover"
-  //                 target="_blank"
-  //                 rel="noopener noreferrer"
-  //                 >
-  //                 Instagram
-  //                 </a>
-  //             </li>
-  //             <li>
-  //                 <a
-  //                 href="https://www.linkedin.com/company/65747941/admin/"
-  //                 className="text-button hover:text-buttonHover"
-  //                 target="_blank"
-  //                 rel="noopener noreferrer"
-  //                 >
-  //                 Linkedin
-  //                 </a>
-  //             </li>
-  //             <li>
-  //                 <a
-  //                 href="https://www.facebook.com/HonMediaSD/"
-  //                 className="text-button hover:text-buttonHover"
-  //                 target="_blank"
-  //                 rel="noopener noreferrer"
-  //                 >
-  //                 Facebook
-  //                 </a>
-  //             </li>
-  //             </ul>
-  //         </div>
-  //         <div className="flex-1 px-3">
-  //             <h2 className="text-2xl font-semibold">Contact</h2>
-  //             <p className="mt-5">HonMediaSD@gmail.com</p>
-  //             <p className="mt-5">San Diego, CA</p>
-  //         </div>
-  //     </div>
-  //     <div className="flex text-center mt-8">&copy; Hon Media 2020</div>
-  // </footer>
 );
 
 export default Footer;
