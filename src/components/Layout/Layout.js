0d7516cc750e5b78a433066563882ac58bf1b12c
Brian Kim <briaansjkim@gmail.com>
2020-10-08 15:10:16 -0400

    working on sidedrawer

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,10 +2,12 @@ import React, { Fragment } from 'react';
 
 import Header from './Header';
 import Footer from './Footer';
+import SideDrawer from './SideDrawer';
 
 const Layout = ({ children }) => (
     <Fragment>
         <Header />
+        <SideDrawer />
         <main className="text-gray-900">{children}</main>
         <Footer />
     </Fragment>
---
+++ src/components/Layout/SideDrawer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import tw from 'tailwind.macro';
+
+const SideDrawer = () => (
+        <div css={tw`float-left border-solid`}>
+            <p>hello</p>
+        </div>
+);
+
+export default SideDrawer;
