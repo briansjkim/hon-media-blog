8b0191f2a16f32881f77cfe75d1d780f19f536cb
Brian Kim <briaansjkim@gmail.com>
2020-10-08 15:39:04 -0400

    init stories

--- src/pages/stories.js
+++ src/pages/stories.js
@@ -1,7 +1,12 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+
+import Header from '../components/Layout/Header';
 
 export default function stories() {
     return (
-        <div>Stories</div>
+        <Fragment>
+            <Header />
+            <div>Stories</div>
+        </Fragment>
     );
 };
