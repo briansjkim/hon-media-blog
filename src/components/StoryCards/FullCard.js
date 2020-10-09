eb9698ae12491372f6088d712ad099561a285189
Brian Kim <briaansjkim@gmail.com>
2020-10-09 13:45:57 -0400

    working on allowing user to view full card

---
+++ src/components/StoryCards/FullCard.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import tw from 'tailwind.macro';
+
+import customerData from '../../assets/data/customer-data';
+
+const FullCard = () => (
+    <p>Hello</p>
+);
+
+export default FullCard;
---
+++ src/pages/card.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from 'react';
+import tw from 'tailwind.macro';
+
+import FullCard from '../components/StoryCards/FullCard';
+
+export default function card() {
+    return (
+        <Fragment>
+            <FullCard />
+        </Fragment>
+    )
+};
