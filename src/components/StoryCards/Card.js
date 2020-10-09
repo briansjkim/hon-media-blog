f8c0c0ba2661006d2164125d79824f1b967ec27e
Brian Kim <briaansjkim@gmail.com>
2020-10-09 13:40:59 -0400

    added more styling and link to full card post

--- src/components/StoryCards/Card.js
+++ src/components/StoryCards/Card.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import tw from 'tailwind.macro';
 
-const Card = ({ children }) => {
-    return (
-        <div
-            css={tw`w-1/4 h-auto mx-24 p-12 rounded-lg border border-solid border-gray-200 text-left`}
-            style={{ boxShadow: '0 10px 28px rgba(0,0,0,.08)' }}
-        >{children}</div>
-    )
-};
+const Card = ({ children }) => (
+    <div
+        css={tw`w-1/4 h-auto mx-24 p-12 rounded-lg border border-solid border-gray-200 text-left`}
+        style={{ boxShadow: '0 10px 28px rgba(0,0,0,.08)' }}
+    >{children}</div>
+);
 
 export default Card;
--- src/components/StoryCards/StoryCards.js
+++ src/components/StoryCards/StoryCards.js
@@ -1,27 +1,37 @@
 import React from 'react';
+import { Link } from 'gatsby';
 import tw from 'tailwind.macro';
 
 import Card from './Card';
 
-const StoryCards = ({ customers }) => (
-    <div css={tw`w-full text-center flex flex-col flex-wrap justify-center md:flex-row md:-mx-3 mt-12`}>
-        {customers.map((customer, idx) =>
-            <Card 
-                key={idx}
-                css={tw`mb-8 float-left`}
-            >
-                <h4 css={tw`text-3xl`}>{customer.title}</h4>
-                <i css={tw`text-xl`}><q>{customer.snippet}</q></i>
-                <div css={tw`text-center mt-8`}>
-                    <img
-                        css={tw`w-56 h-56 mt-10 rounded-full`}
-                        src={customer.customerImage}
-                        alt={customer.customerName}
-                    />
-                </div>
-            </Card>
-        )}
-    </div>
-);
+const StoryCards = ({ customers }) => {
+    return (
+        <div css={tw`w-full text-center flex flex-col flex-wrap justify-center md:flex-row md:-mx-3 mt-12`}>
+            {customers.map((customer, idx) =>
+                <Card 
+                    key={idx}
+                    css={tw`mb-8 float-left`}
+                >
+                    <h4 css={tw`text-3xl`}>
+                        <Link
+                            to="/"
+                            css={tw`px-2 lg:px-4 no-underline text-black hover:text-blue-500 hover:text-opacity-75`}
+                        >
+                            {customer.title}
+                        </Link>
+                    </h4>
+                    <i css={tw`text-xl`}><q>{customer.snippet}</q></i>
+                    <div css={tw`text-center mt-8`}>
+                        <img
+                            css={tw`w-56 h-56 mt-10 rounded-full`}
+                            src={customer.customerImage}
+                            alt={customer.customerName}
+                        />
+                    </div>
+                </Card>
+            )}
+        </div>
+    )
+};
 
 export default StoryCards;
