69c951cf6e6fa7448c3fb3548d999ef6869524de
Brian Kim <briaansjkim@gmail.com>
2020-11-03 14:48:42 -0500

    added auth component to login page

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,10 +1,12 @@
 import React, { Fragment } from 'react';
+import Header from '../components/Layout/Header';
 import Auth from '../components/Auth/Auth';
 
 // auth component will be used in this page
 export default function login() {
     return (
         <Fragment>
+            <Header />
             <Auth />
         </Fragment>
     );
