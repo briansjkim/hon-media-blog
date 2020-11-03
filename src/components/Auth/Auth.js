bcb3acc2258a7868ce1a313f3da1f0383e3ffe7c
Brian Kim <briaansjkim@gmail.com>
2020-11-03 13:13:58 -0500

    init auth component

---
+++ src/components/Auth/Auth.js
@@ -0,0 +1,20 @@
+import React, { Fragment } from 'react';
+
+const Auth = () => {
+    constructor() {
+        super();
+
+        this.state = {
+            user: '',
+            pw: ''
+        };
+    };
+
+    return (
+        <Fragment>
+
+        </Fragment>
+    );
+};
+
+export default Auth;
