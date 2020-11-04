4b298ff051fb68afd40bf1697482ea8de3f64dfe
Josh <josh.sk.kim@gmail.com>
2020-11-03 21:38:58 -0800

    Created separate devtools folder

---
+++ src/components/DevTools/DevToolsDropdown.js
@@ -0,0 +1,73 @@
+import React, { Component } from 'react';
+import tw from 'tailwind.macro';
+import { Link } from 'gatsby';
+
+class DevTools extends Component {
+    constructor() {
+        super();
+      
+        this.state = {
+            showMenu: false,
+        };
+      
+        this.showMenu = this.showMenu.bind(this);
+        this.closeMenu = this.closeMenu.bind(this);
+    }
+    
+    showMenu(event) {
+        event.preventDefault();
+      
+        this.setState({ showMenu: true }, () => {
+            document.addEventListener('click', this.closeMenu);
+        });
+    }
+    
+    closeMenu(event) {
+      
+        if (!this.dropdownMenu.contains(event.target)) {
+        
+            this.setState({ showMenu: false }, () => {
+                document.removeEventListener('click', this.closeMenu);
+            });  
+        
+        }
+    }
+  
+    render() {
+        return (
+            <div
+                css={tw`px-2 lg:px-4 no-underline text-black relative hover:text-blue-500 hover:text-opacity-75`}
+            >
+                <button 
+                    onClick={this.showMenu}
+                >
+                    DEV TOOLS
+                </button>
+          
+            {
+                this.state.showMenu
+                ? (
+                    <div
+                        className="menu"
+                        ref={(element) => {
+                            this.dropdownMenu = element;
+                        }}
+                        css={tw`absolute`}
+                    >
+                        <Link
+                            to='/createpost'
+                            css={tw`text-xs px-2 lg:px-4 no-underline text-black hover:text-blue-500 hover:text-opacity-75 mr-12`}
+                        >
+                            Create Post
+                        </Link>
+                    </div>
+                ) : ( 
+                    null
+                )
+            }
+        </div>
+        );
+    }
+}
+
+export default DevTools;
