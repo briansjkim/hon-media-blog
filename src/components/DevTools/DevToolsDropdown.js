0f8fdfa6a5bdb3d0750a4bfd70db7fe6d6bb4ba3
Brian Kim <briaansjkim@gmail.com>
2020-11-04 15:40:21 -0500

    fixed mouse movement for dropdown menu

--- src/components/DevTools/DevToolsDropdown.js
+++ src/components/DevTools/DevToolsDropdown.js
@@ -11,29 +11,14 @@ class DevTools extends Component {
             showMenu: false,
         };
         
-        this.setWrapperRef = this.setWrapperRef.bind(this);
-        this.handleClickOutside = this.handleClickOutside.bind(this);
         this.handleClick = this.handleClick.bind(this);
+        this.toggleMenu = this.toggleMenu.bind(this);
     };
 
-    componentDidMount() {
-        document.addEventListener('mousedown', this.handleClickOutside);
-    };
-
-    componentWillUnmount() {
-        document.removeEventListener('mousedown', this.handleClickOutside);
-    };
-
-    setWrapperRef(node) {
-        this.wrapperRef = node;
-    };
-
-    handleClickOutside(event) {
-        if (this.wrapperRef && !this.wrapperRef.contains(event.target)) {
-            this.setState({
-                showMenu: false
-            });
-        };
+    toggleMenu() {
+        this.setState(prevState => {
+            return { showMenu: !prevState.showMenu}
+        });
     };
 
     handleClick() {
@@ -47,11 +32,12 @@ class DevTools extends Component {
     render() {
         return (
             <div
-            css={tw`ml-6 mr-6 px-2 lg:px-4 no-underline text-black relative`}
+                aria-hidden="true"
+                onMouseEnter={this.toggleMenu}
+                onMouseLeave={this.toggleMenu}
+                css={tw`ml-6 mr-6 px-2 lg:px-4 no-underline text-black relative`}
             >
                 <button
-                    ref={this.setWrapperRef}
-                    onClick={this.handleClick}
                     css={tw`text-sm font-medium border-none bg-white outline-none hover:text-blue-500 hover:text-opacity-75`}
                 >
                     DEV TOOLS
@@ -66,17 +52,16 @@ class DevTools extends Component {
                         }}
                         css={tw`absolute border-solid bg-white shadow w-full`}
                     >
-                        {/* <ul css={tw`list-none`}> */}
-                            {/* <li> */}
+                        <ul css={tw`list-none`}>
+                            <li>
                                 <Link
                                     to='/createpost'
-                                    // onClick={this.closeMenu}
                                     css={tw`text-xs px-2 lg:px-4 no-underline text-black`}
                                 >
                                     Create Post
                                 </Link>
-                            {/* </li> */}
-                        {/* </ul> */}
+                            </li>
+                        </ul>
                     </div>
                 ) : ( 
                     null
