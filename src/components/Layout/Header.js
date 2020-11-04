562d167c517c9dc8f574e315bb328e32d2952456
Brian Kim <briaansjkim@gmail.com>
2020-11-04 13:15:07 -0500

    removed comment

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -3,9 +3,9 @@ import tw from 'tailwind.macro';
 import { Link } from 'gatsby';
 import { isLoggedIn } from '../Auth/Auth';
 import DevTools from '../DevTools/DevToolsDropdown';
- 
+
 import Logo from '../../assets/images/HonLogo.png';
-// will hold a search feature, home feature (hon media home site), login/sign up?
+
 
 const Header = () => (
     <header css={tw`sticky top-0 bg-white shadow`}>
