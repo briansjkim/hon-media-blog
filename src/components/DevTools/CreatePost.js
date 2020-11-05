950d854f6eae60e0706924b39b9822f749b36d3c
Josh <josh.sk.kim@gmail.com>
2020-11-04 20:05:09 -0800

    Styled text boxes and headers

--- src/components/DevTools/CreatePost.js
+++ src/components/DevTools/CreatePost.js
@@ -18,17 +18,38 @@ class CreatePost extends Component {
     render() {
         return (
             <div>
-                <div css={tw`w-740px h-60px font-poppins font-medium font-h2 text-4xl leading-10`}>
-                    <h2>Create New Post</h2>
+                <div css={tw`tracking-wide w-740px h-60px font-semibold text-4xl text-gray-800 leading-10`}>
+                    Create New Post
                 </div>
                 <form>
-                    <br styles="clear:both" />
-                    <div className="form-group">
-                        <input type="text" id="title" name="title" placeholder="Title" required />
+                    <div 
+                        className="form-group"
+                        css={tw`tracking-wide`}
+                    >
+                        TEXT & CONTENT
+                        <br/>
+                        <input
+                            type="text"
+                            id="title"
+                            name="title"
+                            placeholder="Title"
+                            css={tw`border-2 border-solid border-gray-400 rounded`}
+                            style={{ width: '740px', fontSize: '24px', fontFamily: 'Poppins'  }}
+                            required
+                        />
                     </div>
-                    
                     <div className="form-group">
-                        <textarea type="textarea" id="content" placeholder="Content" maxLength="140" rows="7"></textarea>
+                        <textarea
+                            type="textarea"
+                            id="content"
+                            placeholder="Content"
+                            maxLength="140"
+                            rows="7"
+                            css={tw`border-2 border-solid border-gray-400 rounded`}
+                            style={{ top: '360px', width: '740px', fontSize: '16px', fontFamily: 'Poppins'  }}
+                            required
+                        >
+                        </textarea>
                     </div>
                     
                     <button type="button" id="submit" name="submit" className="btn btn-primary pull-right">Add Post</button>
