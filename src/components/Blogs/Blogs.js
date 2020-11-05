9cc2dbd084e055061615a2c5b2d9d10f347513fd
Brian Kim <briaansjkim@gmail.com>
2020-11-05 14:28:33 -0500

    working on retrieving data from firebase

--- src/components/Blogs/Blogs.js
+++ src/components/Blogs/Blogs.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import tw from "tailwind.macro";
+import axios from '../../axios-instance';
 
 class Blogs extends Component {
     constructor() {
@@ -8,6 +9,12 @@ class Blogs extends Component {
         this.state = {
             blogs: []
         };
+    };
+
+    componentDidMount() {
+        axios.get('/posts.json')
+            .then(response => console.log(response))
+            .catch(error => console.log(error));
     };
 
     render() {
@@ -21,14 +28,5 @@ class Blogs extends Component {
         );
     };
 };
-
-// const Blogs = () => (
-//     <div
-//         css={tw`mx-24 p-12 rounded-lg border border-solid border-gray-200`}
-//         style={{ boxShadow: '0 10px 28px rgba(0,0,0,.08)' }}
-//     >
-//         <h2>Coming Soon!</h2>
-//     </div>
-// );
 
 export default Blogs;
--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,5 +1,5 @@
 import React, { Fragment } from 'react';
-import tw from 'tailwind.macro';
+// import tw from 'tailwind.macro';
 
 import Header from './Header';
 import Footer from './Footer';
