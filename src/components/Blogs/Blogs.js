c0ef45b75aaee683951bf1a98c478951721a7023
Brian Kim <briaansjkim@gmail.com>
2020-11-05 14:17:15 -0500

    converted component to class based to use axios

--- src/components/Blogs/Blogs.js
+++ src/components/Blogs/Blogs.js
@@ -1,13 +1,34 @@
-import React from "react";
+import React, { Component } from "react";
 import tw from "tailwind.macro";
 
-const Blogs = () => (
-    <div
-        css={tw`mx-24 p-12 rounded-lg border border-solid border-gray-200`}
-        style={{ boxShadow: '0 10px 28px rgba(0,0,0,.08)' }}
-    >
-        <h2>Coming Soon!</h2>
-    </div>
-);
+class Blogs extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            blogs: []
+        };
+    };
+
+    render() {
+        return (
+            <div
+                css={tw`mx-24 p-12 rounded-lg border border-solid border-gray-200`}
+                style={{ boxShadow: '0 10px 28px rgba(0,0,0,.08)' }}
+            >
+                <h2>Coming Soon!</h2>
+            </div>
+        );
+    };
+};
+
+// const Blogs = () => (
+//     <div
+//         css={tw`mx-24 p-12 rounded-lg border border-solid border-gray-200`}
+//         style={{ boxShadow: '0 10px 28px rgba(0,0,0,.08)' }}
+//     >
+//         <h2>Coming Soon!</h2>
+//     </div>
+// );
 
 export default Blogs;
--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -13,9 +13,7 @@ const Layout = ({ children }) => (
             css={tw`flex`}
         > */}
             {/* <SideDrawer /> */}
-            <main 
-                className="text-gray-900"
-                css={tw`border-solid`}>{children}</main>
+            <main className="text-gray-900">{children}</main>
         {/* </div> */}
         <Footer />
     </Fragment>
