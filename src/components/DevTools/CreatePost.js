44f31c658e5e6ad3a641e1d5e8d91a180cb93df0
Josh <josh.sk.kim@gmail.com>
2020-11-04 19:14:08 -0800

    Styled header for page

--- src/components/DevTools/CreatePost.js
+++ src/components/DevTools/CreatePost.js
@@ -18,7 +18,7 @@ class CreatePost extends Component {
     render() {
         return (
             <div>
-                <div css={tw`tracking-wider`}>
+                <div css={tw`w-740px h-60px font-poppins font-medium font-h2 text-4xl leading-10`}>
                     <h2>Create New Post</h2>
                 </div>
                 <form>
@@ -28,7 +28,7 @@ class CreatePost extends Component {
                     </div>
                     
                     <div className="form-group">
-                        <textarea type="textarea" id="content" placeholder="Content" maxlength="140" rows="7"></textarea>
+                        <textarea type="textarea" id="content" placeholder="Content" maxLength="140" rows="7"></textarea>
                     </div>
                     
                     <button type="button" id="submit" name="submit" className="btn btn-primary pull-right">Add Post</button>
