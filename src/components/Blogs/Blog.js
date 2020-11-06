fcc9151320ef9835188083702cbef97727d0fd48
Josh <josh.sk.kim@gmail.com>
2020-11-05 16:09:59 -0800

    Formatted card for blog post

--- src/components/Blogs/Blog.js
+++ src/components/Blogs/Blog.js
@@ -1,10 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
+import tw from 'tailwind.macro';
 
-const Blog = ({ blog }) => (
-    <div>
-        <img src={blog.image} width="100" height="100" />
-        <p>{blog.content}</p>
-    </div>
-);
+class Blog extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
 
-export default Blog
+        };
+    }
+
+    render() {
+        return (
+            <div
+                css={tw`bg-white rounded-lg border-solid border-0 overflow-hidden m-4`}
+                style={{ width: '100px', boxShadow: '0px 4px 10px rgba(0, 0, 0, 0.25)' }}
+            >
+                <img
+                    alt="Blog"
+                    src={this.props.blog.image}
+                    width="100" height="100"
+                />
+                <p>{this.props.blog.content}</p>
+            </div>
+        );
+    }
+};
+
+export default Blog;
