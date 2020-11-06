d53d77d19468e8b15e9af4914342ea9088514295
Brian Kim <briaansjkim@gmail.com>
2020-11-06 17:01:11 -0500

    styled blog title section

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -11,26 +11,44 @@ export default function blogs({ location }) {
     return (
         <Fragment>
             <Header />
-            <div>
-                <div>
-                    <h2>{blog.title}</h2>
-                </div>
-                <div css={tw`flex`}>
-                    <div css={tw`float-left mr-10`}>
-                        <p>{blog.name}</p>
-                        <p>{blog.date}</p>
+            <div css={tw`text-center`}>
+                {/* all blog related  */}
+                <div> 
+                    {/* title, author, date, interactions */}
+                    <div>
+                        <h1
+                            style={{ fontWeight: '100', fontSize: '38px', fontFamily: 'Poppins'}}
+                        >
+                            {blog.title}
+                        </h1>
                     </div>
-                    <div css={tw`float-left flex`}>
-                        <Interactions />
+
+                    <div css={tw`flex border-solid`}>
+
+                        <div css={tw`float-left mr-10 border-solid`}>
+                            <p>{blog.name}</p>
+                            <p>{blog.date}</p>
+                        </div>
+
+                        <div css={tw`float-left flex`}>
+                            <Interactions />
+                        </div>
+
                     </div>
+
                 </div>
+
+                {/* content */}
+                <div css={tw`border-solid`}>
+                    <p css={tw`whitespace-pre-line`}>{blog.content}</p>
+                </div>
+
             </div>
-            <div>
-                <p css={tw`whitespace-pre-line`}>{blog.content}</p>
-            </div>
+
             <div css={tw`flex`}>
                 <Interactions />
             </div>
+
         </Fragment>
     )
 };
