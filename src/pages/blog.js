dc498d5f5edd47df0493710cef4381f892b7d845
Brian Kim <briaansjkim@gmail.com>
2020-11-06 17:17:00 -0500

    init formatting of blog post

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -11,41 +11,42 @@ export default function blogs({ location }) {
     return (
         <Fragment>
             <Header />
-            <div css={tw`text-center`}>
-                {/* all blog related  */}
+            <div 
+                css={tw`text-center mb-16`}
+                style={{ fontFamily: 'Poppins'}}
+            >
                 <div> 
-                    {/* title, author, date, interactions */}
-                    <div>
+                    <div css={tw`w-1/2 m-auto text-left`}>
                         <h1
-                            style={{ fontWeight: '100', fontSize: '38px', fontFamily: 'Poppins'}}
+                            style={{ fontWeight: '500', fontSize: '38px'}}
                         >
                             {blog.title}
                         </h1>
                     </div>
-
-                    <div css={tw`flex border-solid`}>
-
-                        <div css={tw`float-left mr-10 border-solid`}>
+                    <div css={tw`w-1/2 flex m-auto`}>
+                        <div 
+                            css={tw`w-1/2 float-left mr-10 text-left`}>
                             <p>{blog.name}</p>
                             <p>{blog.date}</p>
                         </div>
-
-                        <div css={tw`float-left flex`}>
+                        <div css={tw`w-1/2 float-left flex`}>
                             <Interactions />
                         </div>
-
                     </div>
-
                 </div>
-
-                {/* content */}
-                <div css={tw`border-solid`}>
+                <div>
+                    <img 
+                        alt="Blog" 
+                        src={blog.image}
+                        width="400"
+                        height="400"
+                    />
+                </div>
+                <div css={tw`w-1/2 m-auto mt-6 text-left`}>
                     <p css={tw`whitespace-pre-line`}>{blog.content}</p>
                 </div>
-
             </div>
-
-            <div css={tw`flex`}>
+            <div css={tw`flex w-1/2 m-auto`}>
                 <Interactions />
             </div>
 
