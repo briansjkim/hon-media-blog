70fa31b39c57c159e55b8e33947bd16eab9ab149
Brian Kim <briaansjkim@gmail.com>
2020-11-06 14:26:30 -0500

    edited Link placement

--- src/components/Blogs/Blog.js
+++ src/components/Blogs/Blog.js
@@ -1,44 +1,36 @@
-import React, { Component } from 'react';
-import tw from 'tailwind.macro';
-import { navigate, Link } from 'gatsby';
+import React, { Component } from "react";
+import tw from "tailwind.macro";
+import { Link } from "gatsby";
+
+// keep Blog as a class component in order to add 'options' feature for devs
 
 class Blog extends Component {
     constructor(props) {
         super(props);
-        this.state = {
-
-        };
-
-        this.handleClick = this.handleClick.bind(this);
-    }
-
-    // this will go to the blog post itself
-    handleClick() {
-        navigate('/');
+        this.state = {};
     }
 
     render() {
         return (
-            <Link
-                to='/blog'
-                state={{ blog: this.props.blog }}
-            >
-                <div
-                    css={tw`bg-white rounded-lg border-solid border-0 overflow-hidden m-4`}
-                    style={{ width: '200px', boxShadow: '0px 4px 10px rgba(0, 0, 0, 0.25)', cursor: 'pointer' }}
-                    // onClick={this.handleClick}
+            <div
+                css={tw`bg-white rounded-lg border-solid border-0 overflow-hidden m-4`}
+                style={{
+                    width: "200px",
+                    boxShadow: "0px 4px 10px rgba(0, 0, 0, 0.25)",
+                    }}
                 >
-                    <img
-                        alt="Blog"
-                        src={this.props.blog.image}
-                        width="200" height="200"
-                    />
+                <img alt="Blog" src={this.props.blog.image} width="200" height="200" />
+                <Link 
+                    to="/blog" 
+                    state={{ blog: this.props.blog }}
+                    css={tw`cursor-pointer`}
+                >
                     <h3>{this.props.blog.title}</h3>
                     <p>{this.props.blog.content}</p>
-                </div>
-            </Link>
+                    </Link>
+            </div>
         );
-    }
+    };
 };
 
 export default Blog;
