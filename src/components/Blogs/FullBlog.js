b73347df4cde96ab26349ec25600afc658c6c246
Brian Kim <briaansjkim@gmail.com>
2020-11-06 19:03:26 -0500

    fixing structure

--- src/components/Blogs/FullBlog.js
+++ src/components/Blogs/FullBlog.js
@@ -1,49 +1,71 @@
-import React from 'react';
+import React, { Component } from 'react';
 import tw from 'tailwind.macro';
 
 import Interactions from '../DevTools/Interactions';
 
-const FullBlog = ({ blog }) => {
-    return (
-        <div
-                css={tw`text-center mb-16`}
-                style={{ fontFamily: 'Poppins'}}
-            >
-                <div> 
-                    <div css={tw`w-1/2 m-auto text-left`}>
-                        <h1
-                            style={{ fontWeight: '500', fontSize: '38px'}}
-                        >
-                            {blog.title}
-                        </h1>
-                    </div>
-                    <div css={tw`w-1/2 flex m-auto`}>
-                        <div 
-                            css={tw`w-1/2 float-left mr-10 text-left`}>
-                            <p>{blog.name}</p>
-                            <p>{blog.date}</p>
+class FullBlog extends Component {
+    constructor(props) {
+        super(props);
+
+        this.handleLike = this.handleLike.bind(this);
+        this.handleComment = this.handleComment.bind(this);
+        this.handleShare = this.handleShare.bind(this);
+    };
+
+    handleLike() {
+    };
+
+    handleComment() {
+
+    };
+
+    handleShare() {
+
+    };
+
+
+    render() { 
+        return (
+            <div
+                    css={tw`text-center mb-16`}
+                    style={{ fontFamily: 'Poppins'}}
+                >
+                    <div> 
+                        <div css={tw`w-1/2 m-auto text-left`}>
+                            <h1
+                                style={{ fontWeight: '500', fontSize: '38px'}}
+                            >
+                                {this.props.blog.title}
+                            </h1>
                         </div>
-                        <div css={tw` w-1/2 flex justify-end`}>
-                            <Interactions blog={blog} />
+                        <div css={tw`w-1/2 flex m-auto`}>
+                            <div 
+                                css={tw`w-1/2 float-left mr-10 text-left`}>
+                                <p>{this.props.blog.name}</p>
+                                <p>{this.props.blog.date}</p>
+                            </div>
+                            <div css={tw` w-1/2 flex justify-end`}>
+                                <Interactions blog={this.props.blog} />
+                            </div>
                         </div>
                     </div>
+                    <div>
+                        <img 
+                            alt="Blog" 
+                            src={this.props.blog.image}
+                            width="400"
+                            height="400"
+                        />
+                    </div>
+                    <div css={tw`w-1/2 m-auto mt-6 text-left`}>
+                        <p css={tw`whitespace-pre-line`}>{this.props.blog.content}</p>
+                    </div>
+                <div css={tw`flex w-1/2 m-auto`}>
+                    <Interactions blog={this.props.blog} />
                 </div>
-                <div>
-                    <img 
-                        alt="Blog" 
-                        src={blog.image}
-                        width="400"
-                        height="400"
-                    />
-                </div>
-                <div css={tw`w-1/2 m-auto mt-6 text-left`}>
-                    <p css={tw`whitespace-pre-line`}>{blog.content}</p>
-                </div>
-            <div css={tw`flex w-1/2 m-auto`}>
-                <Interactions />
             </div>
-        </div>
-    )
+        );
+    };
 };
 
 export default FullBlog;
--- src/components/DevTools/Interactions.js
+++ src/components/DevTools/Interactions.js
@@ -3,58 +3,32 @@ import tw from 'tailwind.macro';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHeart, faComment, faShare } from '@fortawesome/free-solid-svg-icons';
 
-// possibly change to class component with like, comment, share features
-    // maybe split each feature into its own component?
-class Interactions extends Component {
-    constructor(props) {
-        super(props);
-
-        this.handleLike = this.handleLike.bind(this);
-        this.handleComment = this.handleComment.bind(this);
-        this.handleShare = this.handleShare.bind(this);
-    }
-
-    handleLike() {
-        console.log(this.props.blog)
-    };
-
-    handleComment() {
-
-    };
-
-    handleShare() {
-
-    };
-
-    render() {
-        return (
-            <Fragment>
-                <button
-                        css={tw`bg-purple-500 rounded-full border-0 cursor-pointer mr-4`}
-                        style={{ width: '91px', height: '40px', fontFamily: 'Poppins',  color: 'white', alignItems: 'center', textAlign: 'center' }}
-                        onClick={this.handleLike}
-                    >
-                        <FontAwesomeIcon icon={faHeart} />
-                        {/* {this.props.blog.likes} */}
-                    </button>
-                    <button
-                        css={tw`bg-purple-500 rounded-full border-0 cursor-pointer mr-4`}
-                        style={{ width: '91px', height: '40px', fontFamily: 'Poppins',  color: 'white', alignItems: 'center', textAlign: 'center' }}
-                    >
-                        <FontAwesomeIcon icon={faComment} />
-                        {/* {console.log(this.props.blog.comments)} */}
-                    </button>
-                    <button
-                        css={tw`bg-purple-500 rounded-full border-0 cursor-pointer`}
-                        style={{ width: '91px', height: '40px', fontFamily: 'Poppins',  color: 'white', alignItems: 'center', textAlign: 'center' }}
-                    >
-                        <FontAwesomeIcon icon={faShare} />
-                        {/* {this.props.blog.shares} */}
-                        Share
-                    </button>
-            </Fragment>
-        )
-    }
-};
+const Interactions = ({ blog }) => (
+    <Fragment>
+        <button
+                css={tw`bg-purple-500 rounded-full border-0 cursor-pointer mr-4`}
+                style={{ width: '91px', height: '40px', fontFamily: 'Poppins',  color: 'white', alignItems: 'center', textAlign: 'center' }}
+                onClick={this.handleLike}
+            >
+                <FontAwesomeIcon icon={faHeart} />
+                {/* {this.props.blog.likes} */}
+            </button>
+            <button
+                css={tw`bg-purple-500 rounded-full border-0 cursor-pointer mr-4`}
+                style={{ width: '91px', height: '40px', fontFamily: 'Poppins',  color: 'white', alignItems: 'center', textAlign: 'center' }}
+            >
+                <FontAwesomeIcon icon={faComment} />
+                {/* {console.log(this.props.blog.comments)} */}
+            </button>
+            <button
+                css={tw`bg-purple-500 rounded-full border-0 cursor-pointer`}
+                style={{ width: '91px', height: '40px', fontFamily: 'Poppins',  color: 'white', alignItems: 'center', textAlign: 'center' }}
+            >
+                <FontAwesomeIcon icon={faShare} />
+                {/* {this.props.blog.shares} */}
+                Share
+            </button>
+    </Fragment>
+);
 
 export default Interactions;
