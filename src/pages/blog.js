cb822471b8f2d4363b8c1c087eb20ada0935d296
Brian Kim <briaansjkim@gmail.com>
2020-11-06 19:45:34 -0500

    removed unnecessary imports

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,8 +1,6 @@
 import React, { Fragment } from 'react';
-// import tw from 'tailwind.macro';
 
 import Header from '../components/Layout/Header';
-// import Interactions from '../components/DevTools/Interactions';
 import FullBlog from '../components/Blogs/FullBlog';
 import SideDrawer from '../components/Layout/SideDrawer';
 
