7ebbcd6fe33b56421bfe4d6287c7133f5a34c6d6
Josh <josh.sk.kim@gmail.com>
2020-11-11 15:14:07 -0800

    Added icons and removed outline on click

--- src/components/DevTools/EditModal.js
+++ src/components/DevTools/EditModal.js
@@ -2,12 +2,15 @@ import React, { Component } from 'react';
 import tw from "tailwind.macro";
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faTimes } from '@fortawesome/free-solid-svg-icons';
+import { faTimes, faPen, faTrash } from '@fortawesome/free-solid-svg-icons';
+import { faSquare, faCheckSquare } from '@fortawesome/free-regular-svg-icons';
 
 class EditModal extends Component {
     constructor(props) {
         super(props);
-        this.state = {};
+        this.state = {
+            isFeatured: false
+        };
 
         this.handleClose = this.handleClose.bind(this);
     };
@@ -34,7 +37,7 @@ class EditModal extends Component {
                     <div css={tw`flex justify-center items-center`}>
                         <h2>Options</h2>
                         <button
-                            css={tw`ml-8 border-none bg-white cursor-pointer font-bold`}
+                            css={tw`outline-none ml-8 border-none bg-white cursor-pointer font-bold`}
                             onClick={this.handleClose}
                         >
                             <FontAwesomeIcon 
@@ -44,22 +47,37 @@ class EditModal extends Component {
                     </div>
                     <div css={tw`mb-6 mt-2`}>
                         <button 
-                            css={tw`border-none bg-white cursor-pointer mb-4 text-lg`}
+                            css={tw`outline-none border-none bg-white cursor-pointer mb-4 text-lg`}
                         >
-                            FEATURED
+                            {this.state.isFeatured ?
+                            <FontAwesomeIcon 
+                                icon={faCheckSquare} 
+                            />
+                            :
+                            <FontAwesomeIcon 
+                                icon={faSquare} 
+                            />
+                            }
+                            {' FEATURED'}
                         </button>
                         <br />
                         <button
-                            css={tw`border-none bg-white cursor-pointer text-lg`}
+                            css={tw`outline-none border-none bg-white cursor-pointer text-lg`}
                         >
-                            EDIT
+                            <FontAwesomeIcon 
+                                icon={faPen} 
+                            />
+                            {' EDIT'}
                         </button>
                     </div>
                     <div css={tw`w-1/2 m-auto border-t border-b-0 border-l-0 border-r-0 border-gray-400 border-solid`}>
                         <button 
-                            css={tw`mt-4 mb-4 text-red-600 bg-white border-none cursor-pointer text-lg`}
+                            css={tw`mt-4 mb-4 text-red-600 bg-white outline-none border-none cursor-pointer text-lg`}
                         >
-                            DELETE
+                            <FontAwesomeIcon 
+                                icon={faTrash} 
+                            />
+                            {' DELETE'}
                         </button>
                     </div>
                 </div>
