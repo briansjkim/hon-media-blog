ea3dea49f45704d2a54985137ace4f99e79411b2
Brian Kim <briaansjkim@gmail.com>
2020-12-02 14:10:59 -0500

    edited rendered data

--- src/components/StoryCards/StoryCards.js
+++ src/components/StoryCards/StoryCards.js
@@ -14,15 +14,16 @@ const StoryCards = ({ customers }) => {
                 <Card 
                     key={idx}
                 >
-                    <h4 css={tw`text-3xl`}>
-                        {/* <Link
-                            to="/"
+                    <h4 css={tw`text-left text-3xl`}>
+                        <Link
+                            to={customer.customerSite}
+                            target='_blank'
                             css={tw`px-2 lg:px-4 no-underline text-black hover:text-buttonHover hover:text-opacity-75`}
-                        > */}
+                        >
                             {customer.title}
-                        {/* </Link> */}
+                        </Link>
                     </h4>
-                    <i css={tw`text-xl`}><q>{customer.snippet}</q></i>
+                    <i css={tw`text-xl`}><q>{customer.content}</q></i>
                     <p css={tw`text-gray-600`}>{customer.customerName}, { customer.customerTitle} </p>
                     {customer.customerTitle2 ? <p css={tw`text-gray-600`}>{ customer.customerTitle2} </p> : null }
                     <div css={tw`text-center mt-8`}>
