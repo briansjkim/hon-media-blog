2a26e0c4f73f84459bddec75e4e97bbfbe87cbe5
Josh <josh.sk.kim@gmail.com>
2020-12-04 09:54:37 -0800

    YO THIS MIGHT BE THE ONE

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,54 +1,55 @@
 module.exports = {
     plugins: [
-        {
-            resolve: `gatsby-plugin-postcss`,
-            options: {
-              // Accepts all options defined by `gatsby-plugin-postcss` plugin.
-            },
-        },
-        {
-            resolve: `gatsby-plugin-emotion`,
-            options: {
-              // Accepts all options defined by `babel-plugin-emotion` plugin.
-            },
-        },
-        {
-            resolve: `gatsby-plugin-manifest`,
-            options: {
-              name: 'hon-media-blog',
-              short_name: 'hon-media',
-              start_url: '/',
-              background_color: '#ffffff',
-              theme_color: '#886FDB',
-              display: 'standalone',
-              icon: 'src/assets/images/HonLogo.png'
-            }
-        },
-        {
-          resolve: `gatsby-plugin-create-client-paths`,
-          options: { prefixes: [`/blog/*`] },
-        },
-        `gatsby-plugin-react-helmet`,
-        `gatsby-plugin-fontawesome-css`,
-        {
-          resolve: 'gatsby-plugin-preconnect',
-          options: {
-            domains: ['https://ksr-ugc.imgix.net', 'https://blog-hon-media.firebaseio.com'],
-          },
-        },
-        {
-          resolve: "gatsby-plugin-firebase",
-          options: {
-            credentials: {
-              apiKey: "AIzaSyAI6ggWUoXrN9vrFfR8sdaVqfJEMZn8SNM",
-              authDomain: "blog-hon-media.web.app",
-              databaseURL: "https://blog-hon-media.firebaseio.com",
-              projectId: "blog-hon-media",
-              storageBucket: "blog-hon-media.appspot.com",
-              messagingSenderId: "1:149751637126:web:8b3ab9b331a1c51ebc1d07",
-              appId: "G-G8W84Z7ZC0"
-            }
+      {
+        resolve: "gatsby-plugin-firebase",
+        options: {
+          credentials: {
+            apiKey: "AIzaSyAI6ggWUoXrN9vrFfR8sdaVqfJEMZn8SNM",
+            authDomain: "blog-hon-media.web.app",
+            databaseURL: "https://blog-hon-media.firebaseio.com",
+            projectId: "blog-hon-media",
+            storageBucket: "blog-hon-media.appspot.com",
+            messagingSenderId: "149751637126",
+            appId: "1:149751637126:web:8b3ab9b331a1c51ebc1d07",
+            measurementId: "G-G8W84Z7ZC0"
           }
         }
+      },
+      {
+          resolve: `gatsby-plugin-postcss`,
+          options: {
+            // Accepts all options defined by `gatsby-plugin-postcss` plugin.
+          },
+      },
+      {
+          resolve: `gatsby-plugin-emotion`,
+          options: {
+            // Accepts all options defined by `babel-plugin-emotion` plugin.
+          },
+      },
+      {
+          resolve: `gatsby-plugin-manifest`,
+          options: {
+            name: 'hon-media-blog',
+            short_name: 'hon-media',
+            start_url: '/',
+            background_color: '#ffffff',
+            theme_color: '#886FDB',
+            display: 'standalone',
+            icon: 'src/assets/images/HonLogo.png'
+          }
+      },
+      {
+        resolve: `gatsby-plugin-create-client-paths`,
+        options: { prefixes: [`/blog/*`] },
+      },
+      `gatsby-plugin-react-helmet`,
+      `gatsby-plugin-fontawesome-css`,
+      {
+        resolve: 'gatsby-plugin-preconnect',
+        options: {
+          domains: ['https://ksr-ugc.imgix.net', 'https://blog-hon-media.firebaseio.com'],
+        },
+      },
     ]
 }
