c60ecbdbf169c8d3c1d64bf913ec93a9325598c0
Joshua Kim <josh.sk.kim@gmail.com>
2020-12-07 16:06:26 -0800

    Update seo.js

--- src/components/DevTools/seo.js
+++ src/components/DevTools/seo.js
@@ -1,61 +1,61 @@
-import React from "react"
-import PropTypes from "prop-types"
-import { Helmet } from "react-helmet"
-import { useStaticQuery, graphql } from "gatsby"
+// import React from "react"
+// import PropTypes from "prop-types"
+// import { Helmet } from "react-helmet"
+// import { useStaticQuery, graphql } from "gatsby"
 
-function SEO({ description, lang, pathname }) {
-  const { site } = useStaticQuery(
-    graphql`
-      query {
-        site {
-          siteMetadata {
-            title
-            description
-            author
-            siteUrl
-          }
-        }
-      }
-    `
-  )
+// function SEO({ description, lang, pathname }) {
+//   const { site } = useStaticQuery(
+//     graphql`
+//       query {
+//         site {
+//           siteMetadata {
+//             title
+//             description
+//             author
+//             siteUrl
+//           }
+//         }
+//       }
+//     `
+//   )
 
-  const metaDescription = description || site.siteMetadata.description;
-  const canonical = pathname ? `${site.siteMetadata.siteUrl}${pathname}` : null;
+//   const metaDescription = description || site.siteMetadata.description;
+//   const canonical = pathname ? `${site.siteMetadata.siteUrl}${pathname}` : null;
 
-  return (
-    <Helmet
-      htmlAttributes={{
-        lang,
-      }}
-      link={
-        canonical
-          ? [
-              {
-                rel: "canonical",
-                href: canonical,
-              },
-            ]
-          : []
-      }
-      meta={[
-        {
-          name: `description`,
-          content: metaDescription,
-        },
-        {
-          name: "keywords",
-          content: site.siteMetadata.keywords.join(","),
-        },
-      ]}
-    />
-  )
-}
+//   return (
+//     <Helmet
+//       htmlAttributes={{
+//         lang,
+//       }}
+//       link={
+//         canonical
+//           ? [
+//               {
+//                 rel: "canonical",
+//                 href: canonical,
+//               },
+//             ]
+//           : []
+//       }
+//       meta={[
+//         {
+//           name: `description`,
+//           content: metaDescription,
+//         },
+//         {
+//           name: "keywords",
+//           content: site.siteMetadata.keywords.join(","),
+//         },
+//       ]}
+//     />
+//   )
+// }
 
-SEO.defaultProps = {
-  lang: `en`,
-  meta: [],
-  description: ``,
-  pathname: PropTypes.string,
-}
+// SEO.defaultProps = {
+//   lang: `en`,
+//   meta: [],
+//   description: ``,
+//   pathname: PropTypes.string,
+// }
 
-export default SEO;
+// export default SEO;
