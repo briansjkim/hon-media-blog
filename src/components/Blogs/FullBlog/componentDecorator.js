ff9dd9dcee8789185185b2579c37aba6b63f5bd5
Brian Kim <briaansjkim@gmail.com>
2020-12-09 13:46:27 -0500

    edit warning message

--- src/components/Blogs/FullBlog/componentDecorator.js
+++ src/components/Blogs/FullBlog/componentDecorator.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const componentDecorator = (href, text, key) => (
-    <a href={href} key={key} target="_blank">
+    <a href={href} key={key} target="_blank" rel="noreferrer">
         {text}
     </a>
 );
