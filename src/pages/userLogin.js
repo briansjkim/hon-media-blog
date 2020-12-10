4bc74b844a875af8db853907259102fc75dd799b
Brian Kim <briaansjkim@gmail.com>
2020-12-10 13:35:24 -0500

    changed submit to log in

--- src/pages/userLogin.js
+++ src/pages/userLogin.js
@@ -1,6 +1,6 @@
 import React, { Fragment } from 'react';
 import Header from '../components/Layout/Header';
-import UserLogin from '../components/Auth/UserAcc/UserAcc';
+import UserLogin from '../components/Auth/UserAcc/UserLogin';
 
 export default function userLogin() {
     return (
