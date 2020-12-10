e93363d042dfb1d2f91562e5071473c0484b7f86
Brian Kim <briaansjkim@gmail.com>
2020-12-10 13:27:09 -0500

    removed comment

--- src/pages/login.js
+++ src/pages/login.js
@@ -2,7 +2,6 @@ import React, { Fragment } from 'react';
 import Header from '../components/Layout/Header';
 import Login from '../components/Auth/DevAcc/DevLogin';
 
-// auth component will be used in this page
 export default function login() {
     return (
         <Fragment>
